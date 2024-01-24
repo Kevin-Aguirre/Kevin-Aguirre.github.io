37736a8680ddd70b0001d17bdda3fdc605dfae5f
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-01-24 17:58:22 -0500

    Testing viewing deployed version vs version running locally

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing testing 12
+          testing testing foo
         </p>
         <a
           className="App-link"
