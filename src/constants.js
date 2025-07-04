0ecc1b80d8f26f38a62d5f3d851553ee624f68f2
Kevin Aguirre <kevinaguirre463@gmail.com>
2025-07-04 13:50:23 -0400

    changed about me description

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,4 @@ export const NAME = "Kevin Aguirre";
 export const INTRODUCTION_TITLE = "Software Developer";
 export const INTRODUCTION_EXPERIENCE = "3+ Years Experience";
 
-export const ABOUT_ME_DESCRIPTION = "Hi! My name is Kevin Aguirre. I'm currently a Junior at New York University, Tandon School of Engineering studying Computer Science. I'm passionate about Software Development, Data Science, and Cybersecurity. I'm currently looking for Software Development internships for Summer 2025. On my spare time, I like to work on personal projects, do calisthenics, go on runs, make Arduino circuits, and play video games.";
+export const ABOUT_ME_DESCRIPTION = "Hi! I'm Kevin. I'm a rising senior at New York University, Tandon School of Engineering studying Computer Science. I'm passionate about Full-Stack Software Development and Cybersecurity. I'm currently looking for Software Development internships for Fall 2025, Spring 2026, and Summer 2026. On my spare time, I like to work on personal projects, powerlift, woodwork, grow plants, and play video games.";
